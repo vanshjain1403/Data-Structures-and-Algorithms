e501151812ea83393a68143a7cbfc64829482dc4
Vansh Jain <jainvansh1403.sre@gmail.com>
2026-06-15 13:01:37 +0530

    Studying Arrays

---
+++ Arrays/initialization_one.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a=1,b=2,c=3,d=4,e=5;
+    int arr[] = {1,2,3,4,5};
+    //indexing--[0,1,2,3,4]
+    cout<<arr[2]  ;
+    return 0;
+}
---
+++ Arrays/updation.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x[] = {1,2,5,4,2,4,6,8,9,6,5,3,6};
+    x[7] = 156 ;
+    cout<<x[7]<<endl;
+    return 0;
+}
