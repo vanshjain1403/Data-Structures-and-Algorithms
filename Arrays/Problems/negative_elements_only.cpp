c1900c10e8ad70a587a2acb9c14a44747ae08c26
Vansh Jain <jainvansh1403.sre@gmail.com>
2026-06-15 13:54:10 +0530

    Problem Solving on Arrays

---
+++ Arrays/Problems/negative_elements_only.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    array<int,10> ele={1,-5,3,-4,-7,8,-6,9,-1,4};
+    for(int i = 0;i<10;i++){
+        if(ele[i]<0) cout<<ele[i]<<endl;
+    }
+    return 0;
+}
